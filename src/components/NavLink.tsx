6ea5dfb48d1372560961254da4a1d0de0d4a7b88
Jad Yahya <jadoyahya@gmail.com>
2024-08-10 02:45:28 +0300

    fix: navbar

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,64 +1,37 @@
-import { Fragment, LegacyRef, ReactElement, cloneElement } from "react";
+import { Fragment, ReactElement, cloneElement } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { useHome } from "../context/providers/homepage.provider";
 import useMobile from "../hooks/useMobile";
 import { tw } from "../utils/tw";
 
 type Props = {
   href: string;
   text: string;
+  onClick?: () => void;
   className?: string;
 };
 
-const scrollTo = (ref: LegacyRef<HTMLDivElement> | undefined) => {
-  // @ts-ignore
-  ref?.current?.scrollIntoView({
-    behavior: "smooth"
-  });
-};
-
-const NavLink = ({ href, text, className }: Props) => {
-  const { aboutRef, contactRef, heroRef, productsRef, servicesRef, teamRef } =
-    useHome();
+const NavLink = ({ href, text, onClick, className }: Props) => {
   const isMobile = useMobile();
   const location = useLocation();
 
-  const handleClick = () => {
-    const refsMap: { [key: string]: LegacyRef<HTMLDivElement> | undefined } = {
-      "#hero": heroRef,
-      "#services": servicesRef,
-      "#products": productsRef,
-      "#about": aboutRef,
-      "#contact": contactRef,
-      "#team": teamRef
-    };
-    const ref = refsMap[href];
-    if (ref) scrollTo(ref);
-  };
-
   const isActive = location.pathname === href || location.hash === href;
-
-  const activeStyles = "text-black";
-
-  const boxUnderlineStyle = isActive
-    ? {
-        backgroundImage:
-          "linear-gradient(to right, #00aeef, #00aeef 25%, #000000 25%, #000000 50%, #fff202 50%, #fff202 75%, #ec0c8c 75%, #ec0c8c 100%)"
-      }
-    : {};
 
   const commonProps = {
     className: tw(
       className ??
         "relative py-6 px-2 text-[#333] font-semibold rounded-xl cursor-pointer hover:text-black duration-300 group",
-      isActive && activeStyles
+      isActive && "text-black"
     ),
     children: (
       <Fragment>
         {text}
         {!isMobile && (
           <span
-            style={boxUnderlineStyle}
+            style={{
+              backgroundImage: isActive
+                ? "linear-gradient(to right, #00aeef, #00aeef 25%, #000000 25%, #000000 50%, #fff202 50%, #fff202 75%, #ec0c8c 75%, #ec0c8c 100%)"
+                : ""
+            }}
             className={tw(
               "absolute left-0 bottom-0 h-0.5",
               isActive
@@ -75,7 +48,7 @@ const NavLink = ({ href, text, className }: Props) => {
     href.startsWith("/") || href.startsWith("#") ? (
       <Link to={href} />
     ) : (
-      <button onClick={handleClick} />
+      <button onClick={onClick} />
     );
 
   return cloneElement(element, commonProps);
